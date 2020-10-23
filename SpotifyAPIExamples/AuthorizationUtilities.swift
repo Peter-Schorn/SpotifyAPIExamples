fb07675b6ffe051a25b5ac08f9f70c223a25643d
Peter-Schorn <petervschorn@gmail.com>
2020-10-22 19:10:39 -0500

    First Commit

---
+++ SpotifyAPIExamples/AuthorizationUtilities.swift
@@ -0,0 +1,55 @@
+import Foundation
+import Combine
+import SpotifyWebAPI
+
+/// Retrieves the client id and client secret from the "client_id" and
+/// "client_secret" environment variables, respectively.
+/// Throws a fatal error if either can't be found.
+func getSpotifyCredentialsFromEnvironment() -> (clientId: String, clientSecret: String) {
+    guard let clientdId = ProcessInfo.processInfo.environment["client_id"] else {
+        fatalError("couldn't find 'client_id' in enviornment variables")
+    }
+    guard let clientSecret = ProcessInfo.processInfo.environment["client_secret"] else {
+        fatalError("couldn't find 'client_secret' in enviornment variables")
+    }
+    return (clientId: clientdId, clientSecret: clientSecret)
+}
+
+extension ClientCredentialsFlowManager {
+    
+    /**
+     A convenience method that calls through to `authorize()` and then blocks
+     the thread until the publisher completes. Returns early if the application
+     is already authorized.
+     
+     This method is defined in this app, not in the `SpotifyWebAPI` module.
+
+     - Throws: If `authorize()` finishes with an error.
+     */
+    func waitUntilAuthorized() throws {
+        
+        if self.isAuthorized() { return }
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var authorizationError: Error? = nil
+        
+        let cancellable = self.authorize()
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    authorizationError = error
+                }
+                semaphore.signal()
+            })
+        
+        _ = cancellable  // supress warnings
+        
+        semaphore.wait()
+        
+        if let authorizationError = authorizationError {
+            throw authorizationError
+        }
+        
+    }
+
+}
--- SpotifyAPIExamples/main.swift
+++ SpotifyAPIExamples/main.swift
@@ -1,11 +1,120 @@
-//
-//  main.swift
-//  SpotifyAPIExamples
-//
-//  Created by Peter Schorn on 10/16/20.
-//
-
 import Foundation
+import Combine
+import SpotifyWebAPI
 
-print("Hello, World!")
+var cancellables: Set<AnyCancellable> = []
+let dispatchGroup = DispatchGroup()
 
+// Retrieve the client id and client secret from the environment variables.
+let spotifyCredentials = getSpotifyCredentialsFromEnvironment()
+
+let spotifyAPI = SpotifyAPI(
+    authorizationManager: ClientCredentialsFlowManager(
+        clientId: spotifyCredentials.clientId,
+        clientSecret: spotifyCredentials.clientSecret
+    )
+)
+// Authorize the application.
+try spotifyAPI.authorizationManager.waitUntilAuthorized()
+
+// MARK: - The Application is Now Authorized -
+
+// MARK: Search for Tracks and Albums
+
+dispatchGroup.enter()
+spotifyAPI.search(
+    query: "The Beatles",
+    categories: [.track, .album],
+    market: "US"
+)
+.sink(
+    receiveCompletion: { completion in
+        print("completion:", completion, terminator: "\n\n\n")
+        dispatchGroup.leave()
+    },
+    receiveValue: { results in
+        print("\nReceived results for search for 'The Beatles'")
+        let tracks = results.tracks?.items ?? []
+        print("found \(tracks.count) tracks:")
+        print("------------------------")
+        for track in tracks {
+            print("\(track.name) - \(track.album?.name ?? "nil")")
+        }
+        
+        let albums = results.albums?.items ?? []
+        print("\nfound \(albums.count) albums:")
+        print("------------------------")
+        for album in albums {
+            print("\(album.name)")
+        }
+        
+    }
+)
+.store(in: &cancellables)
+dispatchGroup.wait()
+
+
+// MARK: Retrieve a Playlist
+
+// "This is Jimi Hendrix"
+// https://open.spotify.com/playlist/37i9dQZF1DWTNV753no4ic?si=c4MU5GqCSamTOIKATWVVIQ
+let playlistURI = "spotify:playlist:37i9dQZF1DWTNV753no4ic"
+
+dispatchGroup.enter()
+spotifyAPI.playlist(playlistURI, market: "US")
+    .sink(
+        receiveCompletion: { completion in
+            print("completion:", completion, terminator: "\n\n\n")
+            dispatchGroup.leave()
+        },
+        receiveValue: { playlist in
+            
+            print("\nReceived Playlist")
+            print("------------------------")
+            print("name:", playlist.name)
+            print("link:", playlist.externalURLs?["spotify"] ?? "nil")
+            print("description:", playlist.description ?? "nil")
+            print("total tracks:", playlist.items.total)
+            for track in playlist.items.items.compactMap(\.item) {
+                print(track.name)
+            }
+            
+        }
+    )
+    .store(in: &cancellables)
+dispatchGroup.wait()
+
+
+// MARK: Retrieve all the Episodes in a Show
+
+// "The Joe Rogan Experience
+// https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk?si=I6Q8JgIqS2-Mifh8Ucey_g
+let showURI = "spotify:show:4rOoJ6Egrf8K2IrywzwOMk"
+
+dispatchGroup.enter()
+spotifyAPI.showEpisodes(showURI, market: "US", limit: 50)
+    // Retrive additional pages of results. In this case,
+    // a total of six pages will be retrieved.
+    .extendPages(spotifyAPI, maxExtraPages: 5)
+    .sink(
+        receiveCompletion: { completion in
+            print("completion:", completion, terminator: "\n\n\n")
+            dispatchGroup.leave()
+        },
+        receiveValue: { episodes in
+            
+            let currentPage = (episodes.offset / 50) + 1
+            if currentPage == 1 {
+                print("Received Show Episodes For The Joe Rogan Experience")
+            }
+            print("page \(currentPage) of results:")
+            print("------------------------")
+            for episode in episodes.items {
+                print(episode.name)
+            }
+            print()
+
+        }
+    )
+    .store(in: &cancellables)
+dispatchGroup.wait()
